01467919c80799287a8b28cf2aeb6f8d983abc7b
Ramon Silva <ramon.ssilva92@gmail.com>
2026-04-14 21:44:49 -0300

    Add SQL query to create report for specific criteria

--- 08-aula08/08-2026-2026-04-14_Postgres-aula/ADS-DS3R40/RAMON_SANTOS_SILVA-H751616/exercicio-01/exercicio1.sql
+++ 08-aula08/08-2026-2026-04-14_Postgres-aula/ADS-DS3R40/RAMON_SANTOS_SILVA-H751616/exercicio-01/exercicio1.sql
@@ -1,1 +1,10 @@
 //exercicio1
+
+SELECT cnpj, status, nome, data_atividade, estado, razao_social
+INTO
+	ramon_santos_silva_h751616.relatorio2
+FROM 
+	public.cnpj_brasil
+WHERE
+	estado = 'AC' AND
+	cast(data_atividade as DATE) > '2024-01-01' limit 2000;
